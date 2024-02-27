d5ad30bc944e5a9b9a5bc6406aab28297970e89e
Madalina Gomei <150043594+madalinagomei@users.noreply.github.com>
2024-02-27 22:27:21 +0100

    Update strings.js
    Signed-off-by: Madalina Gomei <150043594+madalinagomei@users.noreply.github.com>

--- lesson1/strings.js
+++ lesson1/strings.js
@@ -1,24 +1,21 @@
-const namee = "Cristi";
-const surname = "Socaci";
+
+const namee = "Madalina";
+const surname = "Gomei";
 const fullName = namee + " " + surname;
 
 console.log("Length", fullName.length);
-
-console.log("toLowerCase", fullName.toLowerCase());
+@@ -8,14 +8,14 @@ console.log("toLowerCase", fullName.toLowerCase());
 console.log("toUpperCase", fullName.toUpperCase());
 
 console.log("indexOf", fullName.indexOf("i"));
-console.log("indexOf", fullName.indexOf("rist"));
-console.log("indexOf", fullName.indexOf("rit"));
+console.log("indexOf", fullName.indexOf("lin"));
+console.log("indexOf", fullName.indexOf("ln"));
 
-console.log("includes", fullName.includes("rist"));
-console.log("includes", fullName.includes("rit"));
+console.log("includes", fullName.includes("lin"));
+console.log("includes", fullName.includes("ln"));
 
-console.log("endsWith", fullName.endsWith("aci"));
-console.log("endsWith", fullName.endsWith("ai"));
+console.log("endsWith", fullName.endsWith("ome"));
+console.log("endsWith", fullName.endsWith("oe"));
 
 console.log("replace", fullName.replace("i", "a"));
 console.log("replace", fullName.replaceAll("i", "a"));
-
-const fullNameSliced = fullName.slice(1, fullName.length);
-console.log("slice", fullNameSliced);
